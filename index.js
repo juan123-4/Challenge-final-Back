e0717d152dd37fff074e17e2bb695b7b656c5873
juan123-4 <juandibarra09@gmail.com>
2025-04-25 16:42:17 +0200

    cors bloqueo

--- index.js
+++ index.js
@@ -17,8 +17,16 @@ const cookieParser = require("cookie-parser");
 const authroutes = require("./routes/authRoutes");
 
 // Habilitar CORS para todas las solicitudes
-app.use(cors({ origin: "*", credentials: true }));
-
+app.use(cors({
+    origin: (origin, callback) => {
+      if (origin === 'http://localhost:5174' || origin === 'http://localhost:5173') {
+        callback(null, true);  // Permite la solicitud si el origen es uno de los dos
+      } else {
+        callback(new Error('Not allowed by CORS'), false);  // Bloquea otros orígenes
+      }
+    },
+    credentials: true  // Asegura que las cookies se envíen con la solicitud
+  }));
 const PORT = process.env.PORT || 3009
 const routes = require('./routes/routes');
 
